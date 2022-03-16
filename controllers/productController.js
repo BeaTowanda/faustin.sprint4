c45c92cf9d91b21de47750419156fda2707e54e0
beaTowanda <bgonzalez.remax@gmail.com>
2022-03-15 22:29:49 -0300

    errores en crud

---
+++ controllers/productController.js
@@ -0,0 +1,39 @@
+
+const fs = require('fs');
+const path = require('path');
+const model = require('../data/model');
+
+let tableName = fs.readFileSync(path.join(__dirname, "../data/productos.json"), "utf-8")
+
+
+
+const { urlencoded } = require("express");
+const controller = {
+    detail: (req, res) => {
+        let id=req.query.id;
+        let producto = model.find(id);
+        /*let producto = model(jsontable).find(id);*/
+        res.render("detallProdNuevo",{producto})
+    },
+    login: (req,res) =>{
+        res.render("login")
+    },
+    register: (req,res) =>{
+        res.render("formularioRegistro")
+    },
+    carrito: (req,res) => {
+        res.render("carritoDeCompras")
+    },
+    finCarrito: (req,res) => {
+        res.render("finCarrito")
+    },
+    altaProducto: (req,res) => {
+        res.render("altaProducto")
+    },
+    ofertas: (req,res) => {
+        res.render("ofertas")
+    },
+    
+};
+
+module.exports = controller;
---
+++ routes/product.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router()
+const productController = require("../controllers/productController")
+
+
+router.get('/detail/:id', productController.detail);
+router.get("/login",productController.login)
+router.get("/register",productController.register);
+router.get("/carrito",productController.carrito);
+router.get("/finCarrito",productController.finCarrito);
+router.get("/altaProducto",productController.altaProducto);
+router.get("/ofertas",productController.ofertas);
+
+module.exports = router;
